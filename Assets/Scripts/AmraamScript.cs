68e2844cafe7cb329fcfe68efcd035a70b373a36
Aldric Shaun Rajesh <145978659+Aldric24@users.noreply.github.com>
2024-04-14 00:13:19 +0200

    Amraam Updated

--- Assets/Scripts/AmraamScript.cs
+++ Assets/Scripts/AmraamScript.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Android.Types;
 using Unity.Burst.CompilerServices;
 using UnityEngine;
 
@@ -9,7 +10,7 @@ public class AmraamScript : MonoBehaviour
     [SerializeField] private float thrustForce;
     [SerializeField] private float maxSpeed;
     [SerializeField] private float thrustDuration;
-    [SerializeField]private bool isBoosting = true;
+    [SerializeField]private bool isBoosting;
     private float boostTimer = 0;
     private Rigidbody2D rb;
     [SerializeField] private ParticleSystem missileParticleEffect;
@@ -18,27 +19,50 @@ public class AmraamScript : MonoBehaviour
     private Vector3 targetDirection;
     [SerializeField] private float raycastDistance = 2f; // Adjust as needed
     [SerializeField] private LayerMask collisionMask;
+    [SerializeField] private float minVelocityThreshold = 1f;
+    [SerializeField] private float baseDecelerationRate = 0.5f;
+    [SerializeField] private float turningDecelerationMultiplier = 2.0f; // Increase deceleration during turns
+                                                                         // Minimum velocity before destroying
+    [SerializeField] private float decelerationRate = 0.5f; // Rate of velocity decrease
     SweepRotation S;
+    [SerializeField] private float acceleration = 10f; // Acceleration rate
     Vector3 lastposition;
+    [SerializeField] private int velocity;
+    [SerializeField] private GameObject acquiredtarget;
+    WeaponsManager targetinginfo;
+    private Vector3 lastKnownTargetPosition;
+    private Vector3 lastKnownTargetVelocity;
+    private float timeOfLastLock;
+    [SerializeField] float radarAngle = 30f;  // Half the angle of the radar cone
+    [SerializeField] float radarRange = 20f;
     void Start()
     {
+        boostTimer = 0;
         rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        guidance();
-        UpdateParticleEffect();
         
-        if (isBoosting)
+
+        if (rb.simulated!=false)
         {
-            BoostPhase();
+            velocity=((int)rb.velocity.magnitude);
+            guidance();
+            UpdateParticleEffect();
+
+            if (isBoosting)
+            {
+                BoostPhase();
+            }
+            else
+            {
+                InertialPhase();
+            }
+            CheckRaycastCollision();
+
         }
-        else
-        {
-            InertialPhase();
-        }
-        CheckRaycastCollision();
+        
     }
     private void guidance()
     {
@@ -62,10 +86,11 @@ public class AmraamScript : MonoBehaviour
     void BoostPhase()
     {
         AlignWithVelocity();
-        // Thrust-based movement (similar to ApplySteeringForce but using targetDirection)
-        //Vector2 force = targetDirection * thrustForce;
-        //rb.AddForce(force);
-        rb.velocity = transform.up * maxSpeed;
+        AlignWithVelocity();
+
+        // Accelerate towards maxSpeed
+        rb.velocity = Vector2.MoveTowards(rb.velocity, transform.up * maxSpeed, acceleration * Time.fixedDeltaTime);
+
         boostTimer += Time.fixedDeltaTime;
         if (boostTimer >= thrustDuration)
         {
@@ -76,20 +101,78 @@ public class AmraamScript : MonoBehaviour
 
     void InertialPhase()
     {
-        // Movement based on current velocity 
-        // ... (Potentially add some simple guidance updates here, e.g., slight course corrections)
-        AlignWithVelocity();
-        // Thrust-based movement (similar to ApplySteeringForce but using targetDirection)
-        //Vector2 force = targetDirection * thrustForce;
-        //rb.AddForce(force);
-        rb.velocity = transform.up * 1;
+        // Determine Angular Velocity (how fast the missile is turning)
+        float angularVelocityMagnitude = Mathf.Abs(rb.angularVelocity);
 
-        //boostTimer += Time.fixedDeltaTime;
-        //if (boostTimer >= thrustDuration)
+        // Calculate Dynamic Deceleration Rate
+        float decelerationRate = baseDecelerationRate;
+        if (angularVelocityMagnitude > 10f) // Adjust this threshold as needed
+        {
+            decelerationRate *= turningDecelerationMultiplier;
+        }
+
+        // Adjust velocity based on dynamic deceleration
+        rb.velocity = transform.up * (rb.velocity.magnitude - decelerationRate * Time.fixedDeltaTime);
+
+        // Self-destruct Logic
+        //if (rb.velocity.magnitude < minVelocityThreshold)
         //{
-        //    isBoosting = false;
-        //    boostTimer = 0; // Reset the timer
+        //    Destroy(gameObject);
         //}
+    }
+    bool RadarSweep()
+    {
+        
+
+        int numRays = 5; // Number of rays within the cone
+        float angleIncrement = radarAngle / numRays;
+
+        for (float angle = -radarAngle; angle <= radarAngle; angle += angleIncrement)
+        {
+            Vector3 rayDir = Quaternion.Euler(0, 0, angle) * transform.up;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDir, radarRange);
+
+            // Customize what qualifies as a "detected" target
+            if (CheckValidCollision(hit.collider) && hit.collider.CompareTag("Adversary"))
+            {
+                Debug.Log("missile radar sees something");
+                target = hit.transform; // Assign the detected object as the target
+                return true;
+            }
+        }
+        return false; // No target detected
+    }
+    IEnumerator checkTargetinginfo(WeaponsManager info)
+    {
+        while (true)
+        {
+            if (info.target == null)
+            {
+                target = null;
+                RadarSweep();
+            }
+            else if (info.target != null)
+            {
+                target = info.target.transform;
+                lastKnownTargetPosition = target.position;
+                lastKnownTargetVelocity = target.GetComponent<Rigidbody2D>().velocity;
+            }
+            else // info.target is null but we have past data
+            {
+                // Estimate new target position based on previous velocity
+                float timeSinceLastLock = Time.time - timeOfLastLock;
+                Vector3 estimatedPosition = lastKnownTargetPosition + (lastKnownTargetVelocity * timeSinceLastLock);
+                targetDirection = (estimatedPosition - transform.position).normalized;
+                RadarSweep();
+            }
+
+            yield return null;
+        }
+        
+        
+
+       
+        
     }
     bool CheckValidCollision(Collider2D collider )
     {
@@ -145,27 +228,7 @@ public class AmraamScript : MonoBehaviour
 
         }
     }
-    //void OnTriggerEnter2D(UnityEngine.Collider2D collision)
-    //{
-    //    if (collision.gameObject.CompareTag("Adversary"))
-    //    {
-    //        Debug.Log("Enemy hit by missile! onject hit " + collision.gameObject.name);
-    //        Destroy(collision.transform.parent.gameObject);
-
-    //    }
-    //    if (collision.gameObject.CompareTag("Player"))
-    //    {
-    //        Debug.Log("Player hit by missile!");
-    //        //Destroy(collision.transform.parent.gameObject);
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("Object hit by missile!");
-    //    }
-    //    // Put a particle effect he
-    //    // {re
-
-    //}
+   
     void UpdateParticleEffect()
     {
         if (rb.velocity != Vector2.zero) // Check if we have any velocity
@@ -188,16 +251,7 @@ public class AmraamScript : MonoBehaviour
     {
         if(parent!=null)
         {
-            
-            
-            if(parent.target!=null)
-            {
-                target = parent.target.transform;
-            }
-            else
-            {
-                target = null;
-            }
+            StartCoroutine(checkTargetinginfo(parent));
         }
         
         gameObject.GetComponent<Rigidbody2D>().simulated=true;
